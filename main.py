d196f4a4ec9a6b6e73d4cbe22bd5dee534a29f3f
varareandarius <darius-cosmin.vararean@endava.com>
2025-07-09 14:46:14 +0300

    Create base pygame

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((124, 124, 124))  
+    pygame.display.flip()  # Update the display
+
+    clock.tick(60)  # Limit to 60 frames per second
+
+pygame.quit()
+exit(0)
