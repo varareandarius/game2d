8d6aa05fa2c86f57afe6f07efc7af421309660a5
varareandarius <darius-cosmin.vararean@endava.com>
2025-07-09 17:07:32 +0300

    Add prime checking

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame
 from player import player_body, player_body_color, player_speed
+from screenmessage import display_message
+from prime import checkPrime  # Assuming checkPrime is defined in prime.py
 
 pygame.init()
 screen = pygame.display.set_mode((1600, 800))
 clock = pygame.time.Clock()
 running = True
+
+isThisPrime = 31
+
+player_speed = 10  # Speed of the player
+
+
 
 x = 300
 y = 300
@@ -22,7 +30,13 @@ while running:
     top_wall = pygame.draw.rect(screen, (0,0,0), (0,1,1600,2), 0)
     bottom_wall = pygame.draw.rect(screen, (0,0,0), (0,798,1600,2), 0)
 
+    character = pygame.draw.rect(screen, player_body_color, player_body, 0, 50)
+
+
     prime_scene = pygame.draw.rect(screen, (0,0,0), (50, 50, 100, 100), 0)
+
+    if character.colliderect(prime_scene):
+        display_message(screen, "Prime status: " + str(checkPrime(isThisPrime)) , (100, 100), color=(255, 255, 255), font_size=30)
 
     palindrome_scene = pygame.draw.rect(screen, (0,0,0), (200, 50, 100, 100), 0)
     
@@ -49,7 +63,7 @@ while running:
     triee_scene = pygame.draw.rect(screen, (0,0,0), (350, 450, 100, 100), 0)
 
     #display the player body
-    character = pygame.draw.rect(screen, player_body_color, player_body, 0, 50)
+    
 
     pygame.display.flip()  # Update the display
 
