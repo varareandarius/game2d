15e1a605477c2fbf92112ae1c8b5bf61e5311ea6
varareandarius <darius-cosmin.vararean@endava.com>
2025-07-09 16:38:37 +0300

    Create scene for homework

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
+from player import player_body, player_body_color, player_speed
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1600, 800))
 clock = pygame.time.Clock()
 running = True
+
+x = 300
+y = 300
+width = 100
+height = 100
 
 while running:
     for event in pygame.event.get():
@@ -11,7 +17,51 @@ while running:
             running = False
 
     screen.fill((124, 124, 124))  
+    left_wall = pygame.draw.rect(screen, (0,0,0), (1,0,2,800), 0)
+    right_wall = pygame.draw.rect(screen, (0,0,0), (1599,0,2,800), 0)
+    top_wall = pygame.draw.rect(screen, (0,0,0), (0,1,1600,2), 0)
+    bottom_wall = pygame.draw.rect(screen, (0,0,0), (0,798,1600,2), 0)
+
+    prime_scene = pygame.draw.rect(screen, (0,0,0), (50, 50, 100, 100), 0)
+
+    palindrome_scene = pygame.draw.rect(screen, (0,0,0), (200, 50, 100, 100), 0)
+    
+    denominator_scene = pygame.draw.rect(screen, (0,0,0), (350, 50, 100, 100), 0)
+
+    position_scene = pygame.draw.rect(screen, (0,0,0), (500, 50, 100, 100), 0)
+
+    word_scene = pygame.draw.rect(screen, (0,0,0), (650, 50, 100, 100), 0)
+
+    bubble_scene = pygame.draw.rect(screen, (0,0,0), (800, 50, 100, 100), 0)
+
+    merge_scene = pygame.draw.rect(screen, (0,0,0), (950, 50, 100, 100), 0)
+
+    fibonacci_scene = pygame.draw.rect(screen, (0,0,0), (1100, 50, 100, 100), 0)
+
+    nth_prime_scene = pygame.draw.rect(screen, (0,0,0), (1250, 50, 100, 100), 0)
+
+    factorial_scene = pygame.draw.rect(screen, (0,0,0), (1400, 50, 100, 100), 0)
+
+    frequency_scene = pygame.draw.rect(screen, (0,0,0), (50, 450, 100, 100), 0)
+
+    power_scene = pygame.draw.rect(screen, (0,0,0), (200, 450, 100, 100), 0)
+
+    triee_scene = pygame.draw.rect(screen, (0,0,0), (350, 450, 100, 100), 0)
+
+    #display the player body
+    character = pygame.draw.rect(screen, player_body_color, player_body, 0, 50)
+
     pygame.display.flip()  # Update the display
+
+    keys = pygame.key.get_pressed()
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and character.colliderect(left_wall) == False:
+        player_body.x -= player_speed
+    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and character.colliderect(right_wall) == False:
+        player_body.x += player_speed
+    if keys[pygame.K_UP] or keys[pygame.K_w] and character.colliderect(top_wall) == False:
+        player_body.y -= player_speed
+    if keys[pygame.K_DOWN] or keys[pygame.K_s] and character.colliderect(bottom_wall) == False:
+        player_body.y += player_speed
 
     clock.tick(60)  # Limit to 60 frames per second
 
