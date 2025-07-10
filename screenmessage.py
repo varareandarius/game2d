cfaa384bef8a754bad16fa4bf1a28bb9911f547f
varareandarius <darius-cosmin.vararean@endava.com>
2025-07-10 09:21:14 +0300

    Add background to message

--- screenmessage.py
+++ screenmessage.py
@@ -6,5 +6,8 @@ def display_message(screen, message, position, color=(255, 255, 255), font_size=
     font = pygame.font.Font(None, font_size)
     text = font.render(message, True, color)
     text_rect = text.get_rect(center=position)
+    # Desenează un dreptunghi negru sub text
+    background_rect = text_rect.inflate(20, 10)  # puțin padding
+    pygame.draw.rect(screen, (0, 0, 0), background_rect)
     screen.blit(text, text_rect)
     
