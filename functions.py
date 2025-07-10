498e0b744c43e158c57ba094cfcaff13a6ef9e2d
varareandarius <darius-cosmin.vararean@endava.com>
2025-07-10 12:39:52 +0300

    Add fibonacci function

--- functions.py
+++ functions.py
@@ -76,3 +76,14 @@ def merge_sort(arr):
         return finalArr
     else:
         return arr
+    
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
